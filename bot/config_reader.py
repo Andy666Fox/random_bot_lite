8051f830a298c2d12a6f45aa1ca454d020d007e2
Andy666Fox <arsale3005@gmail.com>
2025-05-16 21:40:30 +0300

    initial commit

---
+++ bot/config_reader.py
@@ -0,0 +1,9 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import SecretStr
+
+
+class Settings(BaseSettings):
+    bot_token: SecretStr
+    model_config = SettingsConfigDict(env_file=r"bot\.env")
+
+config = Settings()
