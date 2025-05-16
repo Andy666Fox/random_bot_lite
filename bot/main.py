21624ae3deff6a0e7514d838cc53dd2e033d6ba1
Andy666Fox <arsale3005@gmail.com>
2025-05-17 01:41:21 +0300

    random word plug added

--- bot/main.py
+++ bot/main.py
@@ -3,28 +3,27 @@ import logging
 from aiogram import Bot, Dispatcher, types, Router
 from aiogram.filters.command import Command
 
+from random_word import RandomWords
 import random
 
 from keyboards import get_main_keyboard
 from config_reader import config
 
 logging.basicConfig(level=logging.INFO)
+
 bot = Bot(token=config.bot_token.get_secret_value())
 dp = Dispatcher()
 router = Router()
+rw = RandomWords()
 dp.include_router(router)
 
-r = ['vlados chort' for x in range(5)]
-
-# @dp.message(Command("start"))
-# async def cmd_start(message: types.Message):
-#     await message.answer('Hello')
+answers = ['Вот например:', 'А как тебе этот:', 'Что насчет этого:', 'Ну вот собсна:']
 
 @router.message(Command('start'))
 async def send_welcome(message: types.Message):
     await message.answer(
         "Добро пожаловать! Что умеет этот бот?\n" \
-        "Подкидывать вам случайные каналы со всего Telegram!" \
+        "Подкидывать вам случайные каналы со всего Telegram! (если найдет конечно)" \
         "Попробуйте!",
         reply_markup=get_main_keyboard()
     )
@@ -32,9 +31,7 @@ async def send_welcome(message: types.Message):
 @router.message(lambda message: message.text == "Найти канал")
 async def handle_start_button(message: types.Message):
     print(message.chat.id, message.message_id)
-    await bot.delete_message(message.chat.id, message.message_id)
-    await message.answer(
-            f'{random.choice(r)}'
+    await message.answer(f'{random.choice(answers)}\n@{rw.get_random_word()}'
             #reply_markup=types.ReplyKeyboardRemove()
         )
 
