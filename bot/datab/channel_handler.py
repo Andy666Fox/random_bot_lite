49fd16c5b8831ed707210ed871191750c658f43f
Andy666Fox <arsale3005@gmail.com>
2025-05-23 23:35:06 +0300

    minor changes

--- bot/datab/channel_handler.py
+++ bot/datab/channel_handler.py
@@ -3,7 +3,7 @@ import random
 
 
 def get_random_channel():
-    with open('bot\datab\val_channels.json', 'r') as file:
+    with open('bot\\datab\\val_channels.json', 'r') as file:
         data = json.load(file)
 
     return random.choice(list(data.keys()))
