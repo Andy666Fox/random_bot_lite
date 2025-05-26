e20396e4c0c6076ec54f8a624539e1a84c1374ad
Andy666Fox <arsale3005@gmail.com>
2025-05-26 09:59:59 +0300

    db basic CRUD added

--- bot/datab/channel_handler.py
+++ bot/datab/channel_handler.py
@@ -1,7 +1,52 @@
 import json
 import random
+from aiogram import types 
+from models import Channel, Session
+from sqlalchemy.sql.expression import func
 
 
+async def get_efficient_random(session):
+    return session.query(Channel).order_by(func.random()).limit(1).sclar()
+
+
+async def get_db_random_channel():
+    with Session() as session:
+        random_channel = Channel.get_random(session)
+
+        if random_channel:
+            return random_channel.channelnick
+        else:
+            return 'not found'
+        
+async def add_channel(message: types.Message):
+    channel_nick = message.text.split()[-1]
+    with Session() as session:
+        exists = session.query(Channel).filter_by(channelnick=channel_nick).first()
+        if not exists:
+            new_channel = Channel(channelnick=channel_nick, score=0)
+            session.add(new_channel)
+            session.commit()
+            ## todo: log the db interactions
+            return True
+        return False
+    
+async def increment_score(channel_nick: str):
+    with Session() as session:
+        channel = session.query(Channel).filter_by(channelnick=channel_nick).first()
+        if channel:
+            channel.score += 1
+            session.commit()
+            return True
+        return False 
+    
+async def get_top_channels(limit=10):
+    with Session() as session:
+        channels = session.query(Channel).order_by(Channel.score.desc()).limit(limit).all()
+        return [(ch.channelnick, ch.score) for ch in channels]
+
+
+
+# Deprecated
 def get_random_channel():
     with open("bot\\datab\\val_channels.json", "r") as file:
         data = json.load(file)
--- bot/datab/models.py
+++ bot/datab/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import func
 
 Base = declarative_base()
 
@@ -10,6 +11,11 @@ class Channel(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     channelnick = Column(String(100), unique=True, nullable=False)
     score = Column(Integer, default=0)
+
+    # Deprecated
+    @staticmethod
+    def get_random(session):
+        return session.query(Channel).order_by(func.random()).first()
 
 engine = create_engine('sqlite:///channels.db')
 Base.metadata.create_all(engine)
