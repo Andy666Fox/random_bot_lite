5c7139809b628881bab2d5d12e96ee869331f1cd
Andy666Fox <arsale3005@gmail.com>
2025-07-04 21:06:23 +0300

    black formatting

--- bot/crud.py
+++ bot/crud.py
@@ -12,31 +12,35 @@ from tqdm import tqdm
 async def get_random_channel():
     try:
         async with get_session() as session:
-            count_stmt = select(func.count(Channel.id)).where(Channel.channel_status==1)
+            count_stmt = select(func.count(Channel.id)).where(
+                Channel.channel_status == 1
+            )
             count_result = await session.execute(count_stmt)
             total_count = count_result.scalar()
 
             if total_count == 0:
                 return None
-            
-            random_offset = random.randint(0, total_count-1)
 
-            stmt = (select(Channel.channelnick)
-                    .where(Channel.channel_status==1)
-                    .offset(random_offset)
-                    .limit(1))
+            random_offset = random.randint(0, total_count - 1)
+
+            stmt = (
+                select(Channel.channelnick)
+                .where(Channel.channel_status == 1)
+                .offset(random_offset)
+                .limit(1)
+            )
             result = await session.execute(stmt)
             channel_nick = result.scalar_one_or_none()
             return channel_nick
-        
+
     except Exception as e:
-        print(f'Processing failed: {e}')
+        print(f"Processing failed: {e}")
         return None
 
 
 async def insert_channels_from_csv(csv_file_path: str, batch_size=500):
     data = []
-    with open(csv_file_path, 'r', encoding='utf-8') as f:
+    with open(csv_file_path, "r", encoding="utf-8") as f:
         reader = csv.reader(f)
         next(reader)
         for row in reader:
@@ -44,12 +48,12 @@ async def insert_channels_from_csv(csv_file_path: str, batch_size=500):
 
     total_rows = len(data)
     inserted_count = 0
-    connection_uri = db_url.replace("+asyncpg", '')
+    connection_uri = db_url.replace("+asyncpg", "")
     print(connection_uri)
     conn = await asyncpg.connect(connection_uri)
-    with tqdm(total=total_rows, desc='Inserting data...') as pbar:
+    with tqdm(total=total_rows, desc="Inserting data...") as pbar:
         for i in range(0, total_rows, batch_size):
-            batch = data[i:i+batch_size]
+            batch = data[i : i + batch_size]
             values = []
             params = []
 
@@ -62,7 +66,7 @@ async def insert_channels_from_csv(csv_file_path: str, batch_size=500):
                 INSERT INTO channels (channelnick, channel_status)
                 VALUES {','.join(values)}
                 """
-                
+
             try:
                 await conn.execute(query, *params)
                 inserted_count += len(batch)
@@ -71,6 +75,4 @@ async def insert_channels_from_csv(csv_file_path: str, batch_size=500):
                 print(f"\nОшибка при вставке пакета {i}-{i+len(batch)}: {str(e)}")
 
     await conn.close()
-    print(f'Success: {inserted_count} rows inserted')
-        
-
+    print(f"Success: {inserted_count} rows inserted")
--- bot/schemas.py
+++ bot/schemas.py
@@ -3,10 +3,12 @@ from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.ext.declarative import declarative_base
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 
 Base = declarative_base()
 db_url = f'postgresql+asyncpg://{os.getenv("POSTGRES_USER")}:{os.getenv("POSTGRES_PASSWORD")}@postgres_tgrb:5432/{os.getenv("POSTGRES_DB")}'
+
 
 class Channel(Base):
     # Basic db schema, conatain info about, id, channel nickname and channel score (in development)
@@ -16,14 +18,16 @@ class Channel(Base):
     channelnick = Column(String(100), unique=True, nullable=False)
     channel_status = Column(Integer, nullable=False)
 
+
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, autoincrement=True)
     tg_id = Column(String(100), unique=True, nullable=False)
     username = Column(String(100), unique=True, nullable=False)
 
-engine = create_async_engine(db_url,
-                            echo=True, future=True)
+
+engine = create_async_engine(db_url, echo=True, future=True)
+
 
 async def create_tables():
     async with engine.begin() as conn:
